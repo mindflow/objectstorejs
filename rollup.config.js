ad9cfe274dd4d3bedcb750fdf6757e4fb44b5c15
Mindflow <torbjorn.nilsen@gmail.com>
2021-01-02 13:48:19 +1100

    Upgraded rollup.

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
-import multiEntry from 'rollup-plugin-multi-entry';
-import postprocess from 'rollup-plugin-postprocess';
-import uglify from "rollup-plugin-uglify-es";
+import multi from '@rollup/plugin-multi-entry';
+import replace from '@rollup/plugin-replace';
+import { terser } from "rollup-plugin-terser";
 
 export default [{
     input: "src/**/*.js",
@@ -12,10 +12,14 @@ export default [{
         format: "es"
     },
     plugins: [
-        multiEntry(),
-        postprocess([
-            [/(?<=import\s*(.*)\s*from\s*)['"]((?!.*[.]js).*)['"];/, '\'./$2.js\'']
-        ])
+        multi(),
+        replace({
+            'coreutil_v1': 'coreutilv1',
+            'containerbridge_v1': 'containerbridgev1',
+
+            'coreutilv1': './coreutil_v1.js',
+            'containerbridgev1': './containerbridge_v1.js'
+        })
     ]
 },{
     input: "src/**/*.js",
@@ -26,11 +30,15 @@ export default [{
         format: "es"
     },
     plugins: [
-        multiEntry(),
-        postprocess([
-            [/(?<=import\s*(.*)\s*from\s*)['"]((?!.*[.]js).*)['"];/, '\'./$2.js\'']
-        ]),
-        uglify()
+        multi(),
+        replace({
+            'coreutil_v1': 'coreutilv1',
+            'containerbridge_v1': 'containerbridgev1',
+
+            'coreutilv1': './coreutil_v1.js',
+            'containerbridgev1': './containerbridge_v1.js'
+        }),
+        terser()
     ]
 },{
     input: "src/**/*.js",
@@ -42,7 +50,6 @@ export default [{
         format: "cjs"
     },
     plugins: [
-        multiEntry(),
-        //uglify()
+        multi()
     ]
 }]
